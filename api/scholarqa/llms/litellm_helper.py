7bf2f19d9da07f74c472b9ae51c9dccd4953765c
amanpreet692 <amanpreet.singh.x2@gmail.com>
2025-09-05 08:40:03 -0700

    error handling + fallbacks for sqa

--- api/scholarqa/llms/litellm_helper.py
+++ api/scholarqa/llms/litellm_helper.py
@@ -73,7 +73,7 @@ def setup_llm_cache(cache_type: str = "s3", **cache_args):
 
 
 @traceable(run_type="llm", name="batch completion")
-def batch_llm_completion(model: str, messages: List[str], system_prompt: str = None, fallback: Optional[str] = GPT_4o,
+def batch_llm_completion(model: str, messages: List[str], system_prompt: str = None, fallback: Optional[str] = GPT_5_CHAT,
                          **llm_lite_params) -> List[Optional[CompletionResult]]:
     """returns the result from the llm chat completion api with cost and tokens used"""
     fallbacks = [f.strip() for f in fallback.split(",")] if fallback else []
@@ -120,7 +120,7 @@ def batch_llm_completion(model: str, messages: List[str], system_prompt: str = N
 
 
 @traceable(run_type="llm", name="completion")
-def llm_completion(user_prompt: str, system_prompt: str = None, fallback=GPT_4o, **llm_lite_params) -> CompletionResult:
+def llm_completion(user_prompt: str, system_prompt: str = None, fallback=GPT_5_CHAT, **llm_lite_params) -> CompletionResult:
     """returns the result from the llm chat completion api with cost and tokens used"""
     messages = []
     fallbacks = [f.strip() for f in fallback.split(",")] if fallback else []
